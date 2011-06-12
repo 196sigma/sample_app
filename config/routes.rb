afb9bddb97e21443f94ea8ba64d7951c458a0317
Reggie Edwards <196sigma@gmail.com>
2011-06-11 23:24:05 -0400

    Updated

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ SampleApp::Application.routes.draw do
   match '/contact', :to => 'pages#contact' 
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
-  match "/signup" => "users#new"
+  match "/signup", :to => "users#new"
   root :to => "pages#home"
   
   get "pages/home"
